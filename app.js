2d08f073a94313bb8c12f6f2d9d019d305d3b041
benjyz <benjyc@proton.me>
2023-01-12 18:04:23 +0700

    gov. nodes routes

--- app.js
+++ app.js
@@ -10,11 +10,19 @@ app.use('/static', express.static('static'))
 
 // index page
 app.get('/', function(req, res) {
-  res.render('pages/index');
+  res.render('pages/wallet');
 });
 
-app.get('/wallet', function(req, res) {
-  res.render('pages/wallet');
+app.get('/chat', function(req, res) {
+  res.render('pages/chat');
+});
+
+app.get('/nodes', function(req, res) {
+  res.render('pages/nodes');
+});
+
+app.get('/gov', function(req, res) {
+  res.render('pages/gov');
 });
 
 app.get('/settings', function(req, res) {
