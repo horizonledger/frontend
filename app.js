284d2f5d3171307d2248aff27f10b98744a619d5
benjyz <benjyc@proton.me>
2023-01-12 17:16:42 +0700

    wip

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const app = express()
 const port = 8080
+var eta = require("eta")
 
-app.use('/', express.static('static'))
+app.engine("eta", eta.renderFile)
+
+app.set("view engine", "eta")
+
+app.set("views", "./views")
+
+app.get("/", function (req, res) {
+  res.render("template", {
+    favorite: "Eta"
+  })
+})
+
+// app.use('/', express.static('static'))
 
 
 app.listen(port, () => {
